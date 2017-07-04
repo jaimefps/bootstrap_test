bdda042bbf9f02a04290f036c60aca82fcde331a
Jaime Pericas Saez <jaimefps@Jaimes-MacBook-Pro.local>
2017-07-03 17:47:37 -0700

    completed SERVICES section with animation

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/*=============================================================================
+                                    SERVICES
+=============================================================================*/
+// jQuery code
+$(function () {
+  
+  //initiate the WOW plug-in with jQuery
+  // animation fire on scroll 
+  // when the element has the "wow" class
+  // together with an animte.css animation
+  new WOW().init();
+  
+});
